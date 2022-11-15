5c5d252d4eda484b3d44ed822de322374600dba8
Jiri Tomasek <jtomasek@redhat.com>
2022-11-15 15:27:33 +0100

    Initial library and playground with vite

---
+++ packages/lib/src/components/MyComponent.tsx
@@ -0,0 +1,3 @@
+const MyComponent = () => <div>hello worlds</div>;
+
+export default MyComponent;
---
+++ packages/lib/vite.config.ts
@@ -0,0 +1,38 @@
+import { resolve } from 'pathe';
+import { defineConfig } from 'vite';
+import { WatcherOptions } from 'rollup';
+import dts from 'vite-plugin-dts';
+import react from '@vitejs/plugin-react';
+
+const watcherOptions: WatcherOptions = {};
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'NMState utilites library',
+      // the proper extensions will be added
+      fileName: 'nmstate-ui-lib',
+    },
+    watch: watcherOptions,
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['react', 'react-dom'],
+      output: {
+        // Provide global variables to use in the UMD build
+        // for externalized deps
+        globals: {
+          react: 'React',
+        },
+      },
+    },
+  },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+    react(),
+  ],
+});
