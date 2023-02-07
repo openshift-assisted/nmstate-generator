f5fdadb0409f098963e2b5f2a9fb87a13da12ecb
Jiri Tomasek <jtomasek@redhat.com>
2023-02-07 15:25:14 +0100

    Use base.css from @patternfly/react-core
    Instead of having all styles included twice when using css from @patternfly/patternfly
    Remove unused @patternfly/patternfly dependency

--- apps/playground/src/main.tsx
+++ apps/playground/src/main.tsx
@@ -1,8 +1,11 @@
+/* NOTE: Keep the patternfly base.css import at the top so the base css
+   is processed before any component css */
+import '@patternfly/react-core/dist/styles/base.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import '@patternfly/patternfly/patternfly.css';
 import './main.css';
 import './monacoEditorWorker';
 
